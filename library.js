fe95e73e81468f8715370bb1c3c7fc8fb2301c90
Richardw-rich <1178maestro@gmail.com>
2023-07-25 20:19:00 +1200

    basic styling done

--- library.js
+++ library.js
@@ -1,1 +1,25 @@
-console.log('hi')
+let myLibrary = [ 
+    {
+        title: "Kane and Abel",
+        author: "Jeffrey Archer",
+        pages: 656,
+        read: true
+    }
+]
+
+
+function Book (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = () => {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+ 
+}
+
+bookButton = document.getElementById(newBook)
+bookButton.addEventListener('click', () => {
+    
+})
